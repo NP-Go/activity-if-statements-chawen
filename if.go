392e6c8049488a19acadd0db3f970feb1183edc3
Wen Low <chawenmushu@gmail.com>
2022-03-29 11:36:10 +0800

    Last version commit

--- if.go
+++ if.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 	number1 := 11
-	number2 := 11
-	//resultMessage := "No outcome."
-	//Insert your code here
+	number2 := 17
+	// resultMessage := "No outcome."
+	// Insert your code here
 	// fmt.Println(resultMessage)
 	// fmt.Println(number1)
 	// fmt.Println(number2)
-	//Hint: You may wish to make use of strconv.Itoa to convert int to string
+	// Hint: You may wish to make use of strconv.Itoa to convert int to string
 
 	if number1 == number2 {
 		fmt.Println("11 equal")
